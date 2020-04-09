af0e289e8409702b4a02447a3ab08f90da4ab674
SiqiAw <61196468+SiqiAw@users.noreply.github.com>
2020-04-09 22:06:27 +0800

    Update appointment.js

--- appointment.js
+++ appointment.js
@@ -1,10 +1,10 @@
 function formValidation() {
-	var uname = document.appointment.name;
-	var uphone = document.appointment.phone;
-	var email = document.appointment.mail;
-	var time = document.appointment.aTime;
-	var ucount = document.appointment.count;
-	var ukind = document.appointment.kind;
+	var uname = document.registration.name;
+	var uphone = document.registration.phone;
+	var email = document.registration.mail;
+	var time = document.registration.aTime;
+	var ucount = document.registration.count;
+	var ukind = document.registration.kind;
 
 	if (allletter(uname)) {
 	if (allnumberic(uphone)) {
@@ -13,44 +13,43 @@ function formValidation() {
 	}
 	}
 	return false;
-}
+	
+	function allLetter(uname) { 
+		var letters = /^[A-Za-z]+$/;
+			if(uname.value.match(letters)) {
+				return true;
 
+			} else {
 
-function allLetter(uname) { 
-	var letters = /^[A-Za-z]+$/;
-		if(uname.value.match(letters)) {
-			return true;
+			alert('Username must have alphabet characters only');
+			uname.focus();
+				return false;
+			}
+	}
 
-		} else {
+	function allnumeric(uphone) { 
+		var numbers = /^[0-9]+$/;
+			if(uphone.value.match(numbers)) {
+				return true;
 
-		alert('Username must have alphabet characters only');
-		uname.focus();
+			} else {
+
+			alert('Phone number must in numeric characters only');
+			uphone.focus();
+				return false;
+			}
+	}
+
+	function ValidateEmail(email){
+		var emailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+			if(email.value.match(emailformat)) {
+				return true;
+
+			} else {
+
+			alert("You have entered an invalid email address!");
+			email.focus();
 			return false;
-		}
-}
-
-function allnumeric(uphone) { 
-	var numbers = /^[0-9]+$/;
-		if(uphone.value.match(numbers)) {
-			return true;
-
-		} else {
-
-		alert('Phone number must in numeric characters only');
-		uphone.focus();
-			return false;
-		}
-}
-
-function ValidateEmail(email){
-	var emailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-		if(email.value.match(emailformat)) {
-			return true;
-
-		} else {
-
-		alert("You have entered an invalid email address!");
-		email.focus();
-		return false;
-		}
+			}
+	}
 }
